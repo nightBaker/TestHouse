094c6b1bd49dd6be95c533b1fa0c2ca33d667e57
fatpenguino <aidarfeisal@gmail.com>
2018-12-22 23:52:28 +0600

    создание контекста

---
+++ TestHouse.Infrastructure/Database/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestHouse.Infrastructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
