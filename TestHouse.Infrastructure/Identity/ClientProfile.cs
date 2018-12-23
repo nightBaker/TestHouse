b3b1d2435428be8b6d6a7346c1d2699c1145ae6f
fatpenguino <aidarfeisal@gmail.com>
2018-12-23 14:56:58 +0600

    Infrastucture

---
+++ TestHouse.Infrastructure/Identity/ClientProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TestHouse.Infrastructure.Identity
+{
+    public class ClientProfile
+    {
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
