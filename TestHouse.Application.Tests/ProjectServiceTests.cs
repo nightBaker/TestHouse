aba4788ba39f7f7d8f0208d7295c9a3cc7098070
yerassyl.shalabayev <yerassyl.shalabayev@FHR-DEV-003>
2019-01-17 22:06:50 +0600

    project service tests for get all and get by id

--- TestHouse.Application.Tests/ProjectServiceTests.cs
+++ TestHouse.Application.Tests/ProjectServiceTests.cs
@@ -58,5 +58,142 @@ namespace TestHouse.Application.Tests
                 connection.Close();
             }
         }
+
+        [Fact]
+        public async Task GetAllTest()
+        {
+            // In-memory database only exists while the connection is open
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ProjectRespository>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                // Create the schema in the database
+                using (var context = new ProjectRespository(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                // Run the test against one instance of the context
+                using (var repository = new ProjectRespository(options))
+                {
+                    var projectService = new ProjectService(repository);
+                    await projectService.AddProjectAsync("test name 1", "test description 1");
+                    await projectService.AddProjectAsync("test name 2", "test description 2");
+
+
+                }
+
+                // Use a separate instance of the context to verify correct data was saved to database
+                using (var context = new ProjectRespository(options))
+                {
+                    var projectService = new ProjectService(context);
+                    var projects = await projectService.GetAllAsync();
+
+                    Assert.Collection(projects, project =>
+                    {
+                        Assert.Equal(1, project.Id);
+                        Assert.Equal("test name 1", project.Name);
+                        Assert.Equal("test description 1", project.Description);
+                    },
+                    project =>
+                    {
+                        Assert.Equal(2, project.Id);
+                        Assert.Equal("test name 2", project.Name);
+                        Assert.Equal("test description 2", project.Description);
+                    });
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Fact]
+        public async Task GetTest()
+        {
+            // In-memory database only exists while the connection is open
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ProjectRespository>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                // Create the schema in the database
+                using (var context = new ProjectRespository(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                // Run the test against one instance of the context
+                using (var repository = new ProjectRespository(options))
+                {
+                    var projectService = new ProjectService(repository);
+                    await projectService.AddProjectAsync("test name 1", "test description 1");
+
+                    var suitService = new SuitService(repository);
+
+                    await suitService.AddSuitAsync("suit level 1", "suit description 1", 1, 1);
+                    await suitService.AddSuitAsync("suit level 2", "suit description 2", 1, 2);
+                    await suitService.AddSuitAsync("suit level 1.1", "suit description 1.1", 1, 1);
+                    await suitService.AddSuitAsync("suit level 3", "suit description 3", 1, 3);
+
+                }
+
+                // Use a separate instance of the context to verify correct data was saved to database
+                using (var context = new ProjectRespository(options))
+                {
+                    var projectService = new ProjectService(context);
+                    var project = await projectService.GetAsync(1);
+
+                    Assert.Equal(1, project.RootSuit.Id);
+                    Assert.Equal("root", project.RootSuit.Name);
+                    Assert.Equal("root", project.RootSuit.Description);
+
+                    Assert.Collection(project.RootSuit.Suits, suit =>
+                    {
+                        Assert.Equal(2, suit.Id);
+                        Assert.Equal("suit level 1", suit.Name);
+                        Assert.Equal("suit description 1", suit.Description);
+
+                        Assert.Collection(suit.Suits, suit1 =>
+                        {
+                            Assert.Equal(3, suit1.Id);
+                            Assert.Equal("suit level 2", suit1.Name);
+                            Assert.Equal("suit description 2", suit1.Description);
+
+                            Assert.Collection(suit1.Suits, suit2 =>
+                            {
+                                Assert.Equal(5, suit2.Id);
+                                Assert.Equal("suit level 3", suit2.Name);
+                                Assert.Equal("suit description 3", suit2.Description);
+
+                                Assert.Null(suit2.Suits);
+                            });
+                        });
+                    },
+                    suit =>
+                    {
+                        Assert.Equal(4, suit.Id);
+                        Assert.Equal("suit level 1.1", suit.Name);
+                        Assert.Equal("suit description 1.1", suit.Description);
+
+                        Assert.Null(suit.Suits);
+                    });
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
