adb4130998ae9e59ead73db0e7f30169e5015960
fatpenguino <aidarfeisal@gmail.com>
2019-02-07 01:27:47 +0600

    AccountController first edition

--- TestHouse.Web/Startup.cs
+++ TestHouse.Web/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,9 @@ namespace TestHouse.Web
             services.AddScoped<ProjectService>();
             services.AddScoped<SuitService>();
             services.AddScoped<TestCaseService>();
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
