73984c7746019d7a8fc10465948c9127c30673a1
yerassyl.shalabayev <yerassyl.shalabayev@FHR-DEV-003>
2019-05-03 10:32:24 +0600

    project add

--- TestHouse.Web.Blazor/Startup.cs
+++ TestHouse.Web.Blazor/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using TestHouse.Web.Blazor.Services;
 
 namespace TestHouse.Web.Blazor
 {
@@ -7,6 +8,7 @@ namespace TestHouse.Web.Blazor
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ProjectService>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
