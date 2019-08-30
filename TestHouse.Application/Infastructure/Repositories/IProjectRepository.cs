8d53f5e6508bdd599bccdaae7a44beac3f957cec
yerassyl.shalabayev <yerassyl.shalabayev@FHR-DEV-003>
2019-08-30 12:23:27 +0600

    comments

--- TestHouse.Application/Infastructure/Repositories/IProjectRepository.cs
+++ TestHouse.Application/Infastructure/Repositories/IProjectRepository.cs
@@ -8,10 +8,37 @@ namespace TestHouse.Application.Infastructure.Repositories
 {
     public interface IProjectRepository
     {
+        /// <summary>
+        /// Get project aggregate 
+        /// </summary>
+        /// <param name="id">project id</param>
+        /// <returns>Project aggregate</returns>
         Task<ProjectAggregate> GetAsync(long id);
+
+        /// <summary>
+        /// Get project with fully loaded test run (included test cases and so on)
+        /// </summary>
+        /// <param name="id">project id</param>
+        /// <param name="testRunId">test run id</param>
+        /// <returns>Project aggregate</returns>
         Task<ProjectAggregate> GetAsync(long id, long testRunId);
+
+        /// <summary>
+        /// Save changes
+        /// </summary>
+        /// <returns></returns>
         Task SaveAsync();
+
+        /// <summary>
+        /// Add new project aggregate
+        /// </summary>
+        /// <param name="project"></param>
         void Add(ProjectAggregate project);
+
+        /// <summary>
+        /// Get all project aggregates 
+        /// </summary>
+        /// <returns>List of project aggregates</returns>
         Task<List<ProjectAggregate>> GetAllAsync();
     }
 }
